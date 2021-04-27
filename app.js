82d5681591ca3fb5c8c988e9dbc816def70f7006
Christopher Barnes <chris.barnes.2000@me.com>
2021-04-26 21:50:26 -0700

    [Add]: Integrations to Socket.IO

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const app = express();
 //Socket.io has to use the http server
 const server = require("http").Server(app);
 
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
 //Express View Engine for Handlebars
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+//Establish your public folder
+app.use('/public', express.static('public'))
+
 
 app.get("/", (req, res) => {
   res.render("index.handlebars");
